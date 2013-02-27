4ef2ffa4d0e4887eff9aa85d02960ad66f548ee5
Nick Heating Pi <nick@elyob.com>
2013-02-27 16:10:40 +0000

    Push test

--- test.php
+++ test.php
@@ -6,12 +6,14 @@ function callcurl($location) {
     // set URL and other appropriate options
     curl_setopt($ch, CURLOPT_URL, $location);
     curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
     curl_setopt($ch, CURLOPT_TIMEOUT_MS, 1);
     // grab URL and pass it to the browser
-    curl_exec($ch);
+    $result = curl_exec($ch);
     // close cURL resource, and free up system resources
     curl_close($ch);
+    echo $result;
 }
 
 callcurl('http://192.168.1.90/heating/heatingon.php');
